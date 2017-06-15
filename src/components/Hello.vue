ecadfdb97b648d8630d5d7b7ffecca9d4e10b91d
Librae <librae8226@gmail.com>
2017-06-16 04:37:05 +0800

    integrate muse-ui

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,31 +1,102 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <mu-paper class="master-nav">
+      <mu-bottom-nav :value="masterNav" shift @change="navChange">
+        <mu-bottom-nav-item v-for="item in navItems" :key="item.value" :value="item.value" :title="item.title" :icon="item.icon"></mu-bottom-nav-item>
+      </mu-bottom-nav>
+    </mu-paper>
+    <div class="vsteper-container">
+      <mu-stepper :active-step="activeStep" orientation="vertical">
+        <mu-step>
+          <mu-step-label>
+            选择活动地点
+          </mu-step-label>
+          <mu-step-content>
+            <p>
+              在附近选择一个活动的地点, 最好靠近地铁站公交站，已方便交通，附近设施应尽量完善，最重要的是有Wifi
+            </p>
+            <mu-raised-button label="下一步" class="step-button" @click="handleNext" primary></mu-raised-button>
+          </mu-step-content>
+        </mu-step>
+        <mu-step>
+          <mu-step-label>
+            创建一个群组
+          </mu-step-label>
+          <mu-step-content>
+            <p>
+              创建群组，50人左右，以18-25单身青年为主。。。。。
+            </p>
+            <mu-raised-button label="下一步" class="step-button" @click="handleNext" primary></mu-raised-button>
+            <mu-flat-button label="上一步" class="step-button" @click="handlePrev"></mu-flat-button>
+          </mu-step-content>
+        </mu-step>
+        <mu-step>
+          <mu-step-label>
+            宣传活动
+          </mu-step-label>
+          <mu-step-content>
+            <p>
+              多在群里发消息宣传宣传，有事没事多在群里唠唠嗑，确定的话就ok拉
+            </p>
+            <mu-raised-button label="完成" class="step-button" @click="handleNext" primary></mu-raised-button>
+            <mu-flat-button label="上一步" class="step-button" @click="handlePrev"></mu-flat-button>
+          </mu-step-content>
+        </mu-step>
+      </mu-stepper>
+      <p v-if="finished" style="margin: 32px 46px">
+        都完成啦! <a href="javascript:;" @click="reset">重置</a>
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js PWA'
+      activeStep: 0,
+      masterNav: 'home',
+      navItems: [
+        {
+          value: 'home',
+          title: 'Home',
+          icon: 'home'
+        },
+        {
+          value: 'notification',
+          title: 'Notification',
+          icon: 'notifications'
+        },
+        {
+          value: 'history',
+          title: 'History',
+          icon: 'access_time'
+        },
+        {
+          value: 'settings',
+          title: 'Settings',
+          icon: 'settings'
+        }
+      ]
+    }
+  },
+  computed: {
+    finished () {
+      return this.activeStep > 2
+    }
+  },
+  methods: {
+    navChange: function (nav) {
+      this.masterNav = nav
+    },
+    handleNext () {
+      this.activeStep++
+    },
+    handlePrev () {
+      this.activeStep--
+    },
+    reset () {
+      this.activeStep = 0
     }
   }
 }
@@ -33,21 +104,22 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.vsteper-container {
+  max-width: 380px;
+  max-height: 400px;
+  margin: 16px auto;
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
+.step-button {
+  margin-top: 12px;
+  margin-right: 12px;
 }
-
-li {
-  display: inline-block;
-  margin: 0 10px;
+.master-nav {
+  bottom: 0;
+  width: 100%;
 }
-
-a {
-  color: #42b983;
+@media (max-width: 768px) {
+  .master-nav {
+    position: absolute;
+  }
 }
 </style>
