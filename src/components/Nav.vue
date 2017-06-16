320611994794260b9f7ddcdee5ae9d65b042c7f6
Librae <librae8226@gmail.com>
2017-06-16 22:57:25 +0800

    upgrade vue-loader to support deep selector
    override muse-ui badge default style

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <mu-paper class="master-nav">
-      <mu-bottom-nav :value="masterNav" shift @change="navChange">
+    <mu-paper class="nav-master">
+      <mu-bottom-nav :value="navFocus" shift @change="navChange">
         <!--<mu-bottom-nav-item v-for="item in navItems" :key="item.value" :value="item.value" :title="item.title" :icon="item.icon"></mu-bottom-nav-item>-->
         <mu-bottom-nav-item :key="navItems[0].value" :value="navItems[0].value" :title="navItems[0].title" :icon="navItems[0].icon"></mu-bottom-nav-item>
         <mu-bottom-nav-item :key="navItems[1].value" :value="navItems[1].value" :title="navItems[1].title" :icon="navItems[1].icon">
-          <mu-badge v-if="masterNav !== navItems[1].value" content="10" class="badge" circle secondary/>
+          <mu-badge content="10" class="nav-badge" badgeClass="nav-badge-style" circle secondary/>
         </mu-bottom-nav-item>
         <mu-bottom-nav-item :key="navItems[2].value" :value="navItems[2].value" :title="navItems[2].title" :icon="navItems[2].icon"></mu-bottom-nav-item>
         <mu-bottom-nav-item :key="navItems[3].value" :value="navItems[3].value" :title="navItems[3].title" :icon="navItems[3].icon"></mu-bottom-nav-item>
@@ -18,26 +18,26 @@
 export default {
   data () {
     return {
-      masterNav: 'home',
+      navFocus: 'home',
       navItems: [
         {
           value: 'home',
-          title: 'Home',
+          title: '主要',
           icon: 'home'
         },
         {
           value: 'notification',
-          title: 'Notification',
+          title: '消息',
           icon: 'notifications'
         },
         {
           value: 'history',
-          title: 'History',
+          title: '历史',
           icon: 'access_time'
         },
         {
           value: 'settings',
-          title: 'Settings',
+          title: '设置',
           icon: 'settings'
         }
       ]
@@ -47,7 +47,7 @@ export default {
   },
   methods: {
     navChange: function (nav) {
-      this.masterNav = nav
+      this.navFocus = nav
       this.$router.replace({name: nav})
     }
   }
@@ -56,20 +56,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.master-nav {
+.nav-master {
   width: 100%;
   z-index: 1024;
   top: 0;
   bottom: auto;
 }
 
-.badge {
-  bottom: 30px;
-  left: 10px;
+.nav-badge {
+  position: absolute;
+  top: 2px;
+}
+
+.nav-badge >>> .nav-badge-style {
+  width: 16px;
+  height: 16px;
+  font-size: 10px;
 }
 
 @media (max-width: 768px) {
-  .master-nav {
+  .nav-master {
     position: fixed;
     top: auto;
     bottom: 0;
